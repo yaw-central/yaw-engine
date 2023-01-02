f570bd07f339a60c09dbd8f6576067080a794111
Frederic Peschanski <Frederic.Peschanski@lip6.fr>
2023-01-02 16:39:24 +0100

    MTLloader first example

--- src/java/yaw/engine/resources/MtlLoader.java
+++ src/java/yaw/engine/resources/MtlLoader.java
@@ -13,17 +13,21 @@ public class MtlLoader {
     private LoadMode loadMode = LoadMode.LOAD_UNDEFINED;
     private String loadPath = null;
 
-    private String currentMaterial = null;
+    private MtlMaterial currentMaterial = null;
 
-    public MtlLoader() {}
+    private ObjScene objScene;
 
-    public Map<String, MtlMaterial> parseFromFile(String filename) throws IOException {
-        loadMode = LoadMode.LOAD_FROM_FILE;
-        loadPath = filename;
-        return parseFromBufferedReader(new BufferedReader(new FileReader(filename)));
+    public MtlLoader(ObjScene objScene) {
+        this.objScene = objScene;
     }
 
-    public Map<String, MtlMaterial> parseFromResource(String name) throws IOException {
+    public void parseFromFile(String filename) throws IOException {
+        loadMode = LoadMode.LOAD_FROM_FILE;
+        loadPath = filename;
+        parseFromBufferedReader(new BufferedReader(new FileReader(filename)));
+    }
+
+    public void parseFromResource(String name) throws IOException {
         InputStream istream = ObjLoader.class.getResourceAsStream(name);
         if (istream == null) {
             throw new ObjLoader.ParseError("Cannot find resource: "+name);
@@ -31,23 +35,21 @@ public class MtlLoader {
         BufferedReader reader = new BufferedReader(new InputStreamReader(istream));
         loadMode = LoadMode.LOAD_FROM_RESOURCE;
         loadPath = name;
-        return parseFromBufferedReader(reader);
+        parseFromBufferedReader(reader);
     }
 
-    public Map<String, MtlMaterial> parseFromBufferedReader(BufferedReader reader) throws IOException {
+    public void parseFromBufferedReader(BufferedReader reader) throws IOException {
         String[] lines = Utils.readLines(reader);
-        return parseFromLines(lines);
+        parseFromLines(lines);
     }
 
-    public Map<String, MtlMaterial> parseFromLines(String[] lines) {
-        Map<String, MtlMaterial> materials = new HashMap<>();
+    public void parseFromLines(String[] lines) {
         for(int linepos=1; linepos<=lines.length; linepos++) {
-            parseLine(materials, linepos, lines[linepos-1]);
+            parseLine(linepos, lines[linepos - 1]);
         }
-        return materials;
     }
 
-    public void parseLine(Map<String, MtlMaterial> materials, int linepos, String line) {
+    public void parseLine(int linepos, String line) {
         if (line.isEmpty()) {
             return;
         }
@@ -66,36 +68,33 @@ public class MtlLoader {
                 throw new ObjLoader.ParseError("Missing material name", linepos);
             }
             String matName = parts[1];
-            if (materials.containsKey(matName)) {
-                throw new ObjLoader.ParseError("Dupplicate material name: " + matName, linepos);
-            }
             MtlMaterial material = new MtlMaterial(matName);
-            materials.put(matName, material);
-            currentMaterial = matName;
+            objScene.addMaterial(matName, material);
+            currentMaterial = material;
             return;
         } else if (parts[0].equals("Ns")) {
             float shininess = parseMaterialValue("shininess", parts, linepos);
-            materials.get(currentMaterial).shininess = shininess;
+            currentMaterial.shininess = shininess;
             return;
         } else if (parts[0].equals("Ka")) {
             float ambient = parseMaterialValue("ambient", parts, linepos);
-            materials.get(currentMaterial).ambient = ambient;
+            currentMaterial.ambient = ambient;
             return;
         } else if (parts[0].equals("Kd")) {
             float diffuse = parseMaterialValue("diffuse", parts, linepos);
-            materials.get(currentMaterial).diffuse = diffuse;
+            currentMaterial.diffuse = diffuse;
             return;
         } else if (parts[0].equals("Ks")) {
             float specular = parseMaterialValue("specular", parts, linepos);
-            materials.get(currentMaterial).specular = specular;
+            currentMaterial.specular = specular;
             return;
         } else if (parts[0].equals("Ke")) {
             float emissive = parseMaterialValue("emissive", parts, linepos);
-            materials.get(currentMaterial).emissive = emissive;
+            currentMaterial.emissive = emissive;
             return;
         } else if (parts[0].equals("d")) {
             float opacity = parseMaterialValue("opacity", parts, linepos);
-            materials.get(currentMaterial).opacity = opacity;
+            currentMaterial.opacity = opacity;
             return;
         } else {
             // unsupported entry : emit warning ?
--- src/java/yaw/engine/resources/Utils.java
+++ src/java/yaw/engine/resources/Utils.java
@@ -18,4 +18,25 @@ public class Utils {
         return lines.toArray(new String[0]);
     }
 
+    public static String fetchDirname(String path) {
+        String dirname = path.substring(0, path.lastIndexOf('/'));
+        if (dirname == null) {
+            return "";
+        }
+        return dirname;
+    }
+
+    public static String fetchBasename(String path) {
+        String basename = path.substring(path.lastIndexOf('/')+1);
+        if (basename == null) {
+            return "";
+        }
+        return basename;
+    }
+
+    public static String fetchRelativeName(String path, String name) {
+        String dirName = fetchDirname(path);
+        return dirName + "/" + name;
+    }
+
 }
