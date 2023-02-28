a76e443cb5b11b3397fb8585229e55baecd3632a
OlandoB <80422158+OlandoB@users.noreply.github.com>
2023-02-28 13:06:55 +0100

    Add files via upload

--- src/java/yaw/engine/helper/mkshaders.sh
+++ src/java/yaw/engine/helper/mkshaders.sh
@@ -3,8 +3,8 @@
 PYTHON3=python3
 
 echo "* Making skybox vertex shader *"
-$PYTHON3 ../shader_classify.py "yaw.engine.helper" ./originHelperVertShader.vs
+$PYTHON3 ../shader/shader_classify.py "yaw.engine.helper" ./originHelperVertShader.vs
 
 echo "* Making skybox fragment shader *"
-$PYTHON3 ../shader_classify.py "yaw.engine.helper" ./originHelperFragShader.fs
+$PYTHON3 ../shader/shader_classify.py "yaw.engine.helper" ./originHelperFragShader.fs
 
--- src/java/yaw/engine/helper/originHelperVertShader.java
+++ src/java/yaw/engine/helper/originHelperVertShader.java
@@ -1,8 +1,8 @@
 
-// This file is generated from `fragShader.fs` shader program
+// This file is generated from `originHelperVertShader.fs` shader program
 // Please do not edit directly
 package yaw.engine.helper;
 
 public class originHelperVertShader {
-    public final static String SHADER_STRING = "#version 330 core\n\nlayout(location = 0) in vec3 aPosition;\n\nuniform mat4 modelMatrix;\nuniform mat4 viewMatrix;\nuniform mat4 projectionMatrix;\n\nvoid main() {\ngl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(aPosition, 1.0);\n}";
+    public final static String SHADER_STRING = "#version 330 core\n\nlayout(location = 0) in vec3 position;\n\nuniform mat4 modelMatrix;\nuniform mat4 viewMatrix;\nuniform mat4 projectionMatrix;\n\nvoid main() {\n    vec4 mvPos = modelMatrix * vec4(position,1);\n    gl_Position = projectionMatrix * viewMatrix * modelMatrix * mvPos;\n}";
 }
