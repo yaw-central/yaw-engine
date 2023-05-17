b5c229c03baabf86a409888fd27def4c541b5ad7
OlandoB <80422158+OlandoB@users.noreply.github.com>
2023-05-17 10:07:35 +0200

    files managements

--- src/java/yaw/engine/shader/shadowFragShader.java
+++
@@ -1,8 +0,0 @@
-
-// This file is generated from `shadowFragShader.fs` shader program
-// Please do not edit directly
-package yaw.engine.shader;
-
-public class shadowFragShader {
-    public final static String SHADER_STRING = "#version 330\n\nvoid main()\n{\n\n}\n";
-}
--- src/java/yaw/engine/shader/vertShader.java
+++
@@ -1,8 +0,0 @@
-
-// This file is generated from `vertShader.fs` shader program
-// Please do not edit directly
-package yaw.engine.shader;
-
-public class vertShader {
-    public final static String SHADER_STRING = "#version 330 core\n\nlayout(location = 0) in vec3 position;\nlayout(location = 1) in vec2 texCoord;\nlayout(location = 2) in vec3 normal;\n\nout vec3 vNorm;\nout vec3 vPos;\nout vec2 outTexCoord;\nout vec4 vDirectionalShadowSpace;\n\nuniform mat4 projectionMatrix;\nuniform mat4 viewMatrix;\nuniform mat4 modelMatrix;\nuniform mat4 directionalShadowMatrix;\n\nvoid main()\n{\n	vec4 mvPos = modelMatrix * vec4(position, 1.0);\n    gl_Position = projectionMatrix * viewMatrix * mvPos;\n    vNorm = normalize(transpose(inverse(mat3(modelMatrix))) * normal);\n    vPos = mvPos.xyz;\n    vDirectionalShadowSpace = directionalShadowMatrix * mvPos;\n    outTexCoord=texCoord;\n}";
-}
