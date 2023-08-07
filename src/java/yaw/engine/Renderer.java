634c63f9087a94427d8a62d5ff7630421b15db98
Frederic Peschanski <Frederic.Peschanski@lip6.fr>
2023-08-07 12:44:16 +0200

    reformat

--- src/java/yaw/engine/Renderer.java
+++ src/java/yaw/engine/Renderer.java
@@ -1,13 +1,10 @@
 package yaw.engine;
 
+import org.joml.Matrix4f;
 import yaw.engine.camera.Camera;
 import yaw.engine.light.SceneLight;
 import yaw.engine.shader.ShaderManager;
-import yaw.engine.shader.ShaderProgram;
-import yaw.engine.shader.fragShader;
-import yaw.engine.shader.vertShader;
 import yaw.engine.skybox.Skybox;
-import org.joml.Matrix4f;
 
 import static org.lwjgl.opengl.GL11.*;
 
@@ -17,12 +14,6 @@ import static org.lwjgl.opengl.GL11.*;
  * The shader allows to describe the absorption, the diffusion of the light, the texture to be used, the reflections of the objects, the shading, etc ...
  */
 public class Renderer {
-    /**
-     * Basic rendering.
-     */
-    public void init() throws Exception {
-        /* ?? */
-    }
 
     /**
      * Specific rendering.
@@ -43,13 +34,6 @@ public class Renderer {
             pCamera.updateProjectionMat();
         }
 
-        //Debug
-        /*  int err = GL11.GL_NO_ERROR;
-        if ((err = GL11.glGetError()) != GL11.GL_NO_ERROR) {
-
-            System.out.println(err);
-        }*/
-
         /* Initialization of the window we currently use. */
         glViewport(0, 0, Window.getWidth(), Window.getHeight());
 
@@ -61,28 +45,21 @@ public class Renderer {
         glEnable(GL_DEPTH_TEST);
         glDepthFunc(GL_LESS);       /* Specify the value used for depth buffer comparisons. */
         glClearDepth(1); /* GlClearDepth specifies the depth value used by glClear to clear the depth buffer.
-                                                   The values ​​specified by glClearDepth are set to range [0.1].*/
+                                                   The values specified by glClearDepth are set to range [0.1].*/
 
         glDisable(GL_BLEND);
 
         glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-        /* Rendering of the light. */
+        /* Rendering of the scene */
+        pSceneVertex.render(pCamera, shaderManager);
 
-
-        /* Init Objects. */
-        //pSceneVertex.initMesh();
-
-        /* Update objects
-        XXX useless?  sc.update(); */
-
-        /* Rendering of the object. */
-        pSceneVertex.render(pCamera,shaderManager);
+        /* Rendering of lights */
         pSceneLight.render(new Matrix4f().identity(), shaderManager);
-        /* Cleans all services. */
 
+        /* skybox */
         if (pSkybox != null) {
-            if (pSkybox.init == false) {
+            if (!pSkybox.init) {
                 try {
                     pSkybox.init();
                 } catch (Exception e) {
